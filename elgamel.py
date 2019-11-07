1ddc14c2afe493b2c23272cc6c573c0c953ef33c
peter valentine <valentpj@clarkson.edu>
2019-11-07 11:27:05 -0500

    added description fo efficient generator, still need to implement

--- elgamel.py
+++ elgamel.py
@@ -3,6 +3,13 @@ from crypto.a2.rsa import is_prime
 
 
 def inefficient_generator(upper_bound):
+  """
+  fast way to do a generator
+  let p = 2q + 1 where q = (p-1) / 2
+  test: if g ≠ 1 and g^2 ≠ 1 and g^q ≠ 1 then g is a generator
+  """
+
+
     pass
 
 
@@ -18,5 +25,8 @@ def generate_keys(key_length: int):
     p = randint(lower, upper) | 1
     while not is_prime(p):
         p = randint(lower, upper) | 1
+    
 
+    a = randint(1, p - 1) #this is the secret key
+    
 
