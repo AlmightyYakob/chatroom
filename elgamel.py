b1b5693c3865c4b33b2a1ba49654f06398f69547
Jacob Nesbitt <jjnesbitt2@gmail.com>
2019-11-07 16:17:13 -0500

    Formatting and add return

--- elgamel.py
+++ elgamel.py
@@ -2,24 +2,24 @@ from random import randint
 from crypto.a2.rsa import is_prime
 
 
-def inefficient_generator(upper_bound):
+def inefficient_generator(p):
     """
     fast way to do a generator
-    let p = 2q + 1 where q = (p-1) / 2 (p is upper_bound in our case)
+    let p = 2q + 1 where q = (p-1) / 2
     test: if g ≠ 1 and g^2 ≠ 1 and g^q ≠ 1 then g is a generator
-    note: there are gonig to be p/2 valid generators so just picking 
+    note: there are gonig to be p/2 valid generators so just picking
     a random number to test is probably the best way to do it
     """
-    q = int((upper_bound - 1) / 2)
+    q = int((p - 1) / 2)
 
-    is_generator = False
-    while is_generator == False:
-      g = randint(2, upper_bound -1) #pick a random number to test, dont include 1
-      if(pow(g, 2, upper_bound) != 1 and pow(g, q, upper_bound)!= 1):
-        is_generator = True
-
+    valid = False
+    while not valid:
+        g = randint(2, p - 1)  # pick a random number to test, dont include 1
+        if pow(g, 2, p) != 1 and pow(g, q, p) != 1:
+            valid = True
 
     return g
+
 
 def generate_keys(key_length: int):
     """
@@ -33,10 +33,9 @@ def generate_keys(key_length: int):
     p = randint(lower, upper) | 1
     while not is_prime(p):
         p = randint(lower, upper) | 1
-    
-    g = inefficient_generator(p) #get the generator
-    a = randint(1, p - 1) #this is the secret key
-    b = pow(g,a,p)
-    print("A: ", a, "B: ", b, "G: ", g)
 
-generate_keys(7)
+    g = inefficient_generator(p)  # get the generator
+    a = randint(1, p - 1)  # this is the secret key
+    b = pow(g, a, p)
+
+    return (p, g, b, a)
